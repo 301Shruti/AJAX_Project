dde63f1c4766f9dc2242b76c2c3cad1e2c6d8717
301Shruti <shrutipathseekers@gmail.com>
2023-08-11 21:08:42 +0530

    scroll behaviour

--- main.js
+++ main.js
@@ -1,24 +1,22 @@
-document.getElementById('search').addEventListener("search", function (event) {
+document.getElementById('search').addEventListener("click", function (event) {
     event.preventDefault(); // Prevent form submission
+    document.getElementById('property').style.display = "block";
+    document.getElementById("property").scrollIntoView({ behavior: "smooth" });
 
     let foodName = document.getElementById("foodName").value.trim();
-    let type = document.getElementById('type').value.trim();
-   
-
+    // let type = document.getElementById('type').value.trim();
     const xhr = new XMLHttpRequest();
     const urlAll = `https://indnutrientsapi.tech/food`;
     const urlName = `https://indnutrientsapi.tech/food/name/${foodName}`;
     // const urlType = `https://indnutrientsapi.tech/food?type=${type}`;
     let url;
-   
+
     if (foodName !== "") {
-        url=urlName;
+        url = urlName;
     }
-    // else if (type !== "") {
-    //    url=urlType;
-    // }
+
     else {
-        url=urlAll;
+        url = urlAll;
     }
     xhr.open('GET', url, true);
     xhr.onreadystatechange = () => {
@@ -31,23 +29,25 @@ document.getElementById('search').addEventListener("search", function (event) {
                 let output = "";
                 for (let i = 0; i < res.length; i++) {
                     output += `
-                    <div class="box">
-                        <h4>Food Name: ${res[i].food_name}</h4>
-                        <ul>
-                            <li>Calories: ${res[i].calories}</li>
-                            <li>Type of Food: ${res[i].type}</li>
-                            <li>Quantity: ${res[i].quantity}</li>
-                            <li>Food Nutrition: ${res[i].food_nutrition}</li>
-                        </ul>
+                    <div class="box" >
+                        <h3>Nutrition Information for ${res[i].food_name}</h3>
+                        <p>Calories: ${calories} kcal</p>
+                            <p>Protein: ${protein} g</p>
+                            <p>Carbohydrates: ${carbs} g</p>
+                            <p>Fat: ${fat} g</p>
                     </div>`;
                 }
                 console.log(output);
                 document.getElementById('property').innerHTML = output;
-            } 
-            // else {
-            //     console.error(`Error: ${xhr.status} - ${xhr.statusText}`);
-            // }
+            }
+
         }
     };
     xhr.send();
 });
+ // <ul>
+                        //     <li>Calories: ${res[i].calories}</li>
+                        //     <li>Type of Food: ${res[i].type}</li>
+                        //     <li>Quantity: ${res[i].quantity}</li>
+                        //     <li>Food Nutrition: ${res[i].food_nutrition}</li>
+                        // </ul>
